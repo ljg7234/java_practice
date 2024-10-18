950ccab015f4e80cc539731ab49f4b4b47478a42
ljg7234 <ljg7234@naver.com>
2024-10-18 16:15:53 +0900

    first commit

---
+++ practice1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class practice1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 2;
+        int result = i + j;
+        System.out.println(result);
+
+    }
+}
