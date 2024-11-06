3c2a49224d6e468fd08551c8b756ff19d6779f07
ljg7234 <ljg7234@naver.com>
2024-11-06 17:19:24 +0900

    first commit

---
+++ Practice4.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        String str1 = "" +
+                "{\n"+
+                "\t\"id\":\"winter\",\n" +
+                "\t\"name\":\"눈송이\"\n" +
+                "}";
+
+        String str2 = """
+                {
+                    "id":"winter",
+                    "name":"눈송이"
+                }
+                """;
+
+        String str3 = "hello" + "hi";
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
+
+/*safsafdafsfd
+
+ */
