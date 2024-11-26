ff20b99062edbdc87cc59ccc7f0b557acaf0e2ab
ljg7234 <ljg7234@naver.com>
2024-11-26 16:30:44 +0900

    first commit

---
+++ Practice17.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Practice17 {
+    public static void main(String[] args) {
+        int score = (int)(Math.random() * 20) + 81;
+        System.out.println("점수 :" + score);
+
+        String grade;
+
+        if (score >= 90) {
+            if(score >= 95){
+                grade = "A";
+            } else {
+                grade = "B";
+            }
+        } else {
+            if(score > 85){
+                grade = "C";
+            } else {
+                grade = "D";
+            }
+        }
+
+        System.out.println("학점 :" + grade);
+    }
+
+}
